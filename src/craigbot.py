a1bf19b47b26966b9012ae1cc2abed4deab0cdd4
Kris Wallperington <Honibannchi@gmail.com>
2016-08-19 01:42:35 -0700

    Add some boilerplate

---
+++ src/craigbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import discord
+import asyncio
+
+class CraigBot():
+    def __init__(self):
+        self.client = discord.Client()
+        self.client.run("MjEwNTkzOTA1MjQwOTY1MTIx.CphKqA.h_JmSN1GObrnseS1C3-QoSniX8Y")
+
+def main():
+    CraigBot()
+
+if __name__ == "__main__":
+    main()
