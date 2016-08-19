1d824a5a7440e813514a709e54807ca2d22c03be
Kris Wallperington <Honibannchi@gmail.com>
2016-08-19 01:45:52 -0700

    Remove token

--- src/craigbot.py
+++ src/craigbot.py
@@ -6,7 +6,7 @@ import asyncio
 class CraigBot():
     def __init__(self):
         self.client = discord.Client()
-        self.client.run("MjEwNTkzOTA1MjQwOTY1MTIx.CphKqA.h_JmSN1GObrnseS1C3-QoSniX8Y")
+        self.client.run("MjEwNTkzOTA1MjQwOTY1MTIx.CphYgQ._0rplIifnnLA9ItMqfF5mb-GrqE")
 
 def main():
     CraigBot()
